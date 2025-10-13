ae82de425d0c177846acb29f74e3e50afa791447
bertino2 <berolbertindjomo@gmail.com>
2025-10-13 21:49:42 +0200

    gestion de la recherche

--- frontend/src/ShowChant.jsx
+++ frontend/src/ShowChant.jsx
@@ -11,7 +11,7 @@ export default function ShowChant() {
   const [user, setUser] = useState(null);
   const [chants, setChants] = useState([]);
   const [showAddForm, setShowAddForm] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
   const [filteredChants, setFilteredChants] = useState([]);
   const navigate = useNavigate();
 
@@ -22,7 +22,7 @@ export default function ShowChant() {
     fetchChants();
   }, []);
 
-   const fetchChants = async () => {
+  const fetchChants = async () => {
     try {
       const res = await api.get("/api/chants");
       setChants(res.data);
@@ -31,44 +31,42 @@ export default function ShowChant() {
     }
   };
 
-
- //  Filtrage automatique à chaque saisie
+  //  Filtrage automatique à chaque saisie
   useEffect(() => {
     const term = searchTerm.toLowerCase();
-    const filtered = chants.filter((chant) =>
-        
-        (chant.titre?.toLowerCase().includes(term) ||
-        chant.auteur?.toLowerCase().includes(term))
+    const filtered = chants.filter(
+      (chant) =>
+        chant.titre?.toLowerCase().includes(term) ||
+        chant.auteur?.toLowerCase().includes(term)
     );
     setFilteredChants(filtered);
   }, [searchTerm, chants]);
 
   return (
     <div className="min-h-screen bg-gray-50">
-    
-     <Header navigate={navigate} user={user} />
+      <Header navigate={navigate} user={user} />
       {/* Contenu principal */}
       <main className="container mx-auto p-6 sm:p-8 md:p-10 max-w-6xl">
-      <div className="flex flex-col sm:flex-row items-center justify-between mb-6 gap-4">
-  {/* Champ de recherche */}
-  <input
-    type="text"
-    value={searchTerm}
-    onChange={(e) => setSearchTerm(e.target.value)}
-    placeholder="Rechercher un chant ..."
-    className="w-full sm:w-64 border border-gray-400 rounded-lg py-2 px-3 text-base text-gray-800 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
-  />
-        {/* Bouton Ajouter pour admin */}
-        {(user?.role === "admin" || user?.role === "manager") && (
-          // <div className="flex justify-end mb-6">
+        <div className="flex flex-col sm:flex-row items-center justify-between mb-6 gap-4">
+          {/* Champ de recherche */}
+          <input
+            type="text"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            placeholder="Rechercher un chant ..."
+            className="w-full sm:w-64 border border-gray-400 rounded-lg py-2 px-3 text-base text-gray-800 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+          />
+          {/* Bouton Ajouter pour admin */}
+          {(user?.role === "admin" || user?.role === "manager") && (
+            // <div className="flex justify-end mb-6">
             <button
               onClick={() => setShowAddForm(true)}
               className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-5 rounded-lg transition w-full sm:w-auto"
             >
               + Ajouter un chant
             </button>
-         
-        )} </div>
+          )}{" "}
+        </div>
 
         {/* Formulaire d'ajout si actif */}
         {showAddForm && (
@@ -80,40 +78,44 @@ export default function ShowChant() {
 
         {/* Grille des chants */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {filteredChants.length > 0 ? filteredChants.map((c) => (
-            <a href={`/showChants/${c._id}`} key={c._id} className="block">
-              <div className="bg-white shadow-lg rounded-2xl p-5 flex flex-col justify-between cursor-pointer hover:shadow-xl transition">
-                <h3 className="font-bold text-xl text-gray-800 mb-2">
-                  {c.titre}
-                </h3>
-                <p className="text-gray-500 italic mb-4">
-                  Auteur : {c.auteur || "Inconnu"}
-                </p>
+          {filteredChants.length > 0 ? (
+            filteredChants.map((c) => (
+              <a href={`/showChants/${c._id}`} key={c._id} className="block">
+                <div className="bg-white shadow-lg rounded-2xl p-5 flex flex-col justify-between cursor-pointer hover:shadow-xl transition">
+                  <h3 className="font-bold text-xl text-gray-800 mb-2">
+                    {c.titre}
+                  </h3>
+                  <p className="text-gray-500 italic mb-4">
+                    Auteur : {c.auteur || "Inconnu"}
+                  </p>
 
-                {(user?.role === "admin" || user?.role === "manager") && (
-                  <div className="flex space-x-4">
-                    <Link
-                      to={`/editChant/${c._id}`}
-                      className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg transition"
-                    >
-                      Éditer
-                    </Link>
+                  {(user?.role === "admin" || user?.role === "manager") && (
+                    <div className="flex space-x-4">
+                      <Link
+                        to={`/editChant/${c._id}`}
+                        className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg transition"
+                      >
+                        Éditer
+                      </Link>
 
-                    <button
-                      onClick={(e) => {
-                        e.preventDefault(); 
-                        e.stopPropagation(); 
-                        handleDelete(c._id, navigate,fetchChants);
-                      }}
-                      className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg transition"
-                    >
-                      Supprimer
-                    </button>
-                  </div>
-                )}
-              </div>
-            </a>
-          )): <div className="text-center">Aucun chant trouvé</div>}
+                      <button
+                        onClick={(e) => {
+                          e.preventDefault();
+                          e.stopPropagation();
+                          handleDelete(c._id, navigate, fetchChants);
+                        }}
+                        className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-lg transition"
+                      >
+                        Supprimer
+                      </button>
+                    </div>
+                  )}
+                </div>
+              </a>
+            ))
+          ) : (
+            <div className="text-center">Aucun chant trouvé</div>
+          )}
         </div>
       </main>
     </div>
--- frontend/src/components/AdminPage.jsx
+++ frontend/src/components/AdminPage.jsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from 'react'
-import Header from './Header';
+import React, { useEffect, useState } from "react";
+import Header from "./Header";
 import axios from "axios";
-import { Link, useNavigate } from 'react-router-dom';
-import { formatDate } from '../utils/FormatDate';
-import { handleDeleteUser } from '../services/HandleDeleteUser';
+import { Link, useNavigate } from "react-router-dom";
+import { formatDate } from "../utils/FormatDate";
+import { handleDeleteUser } from "../services/HandleDeleteUser";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 const api = axios.create({ baseURL: apiUrl });
 function AdminPage() {
   const [user, setUser] = useState(null);
-  const [myUsers,setMyUsers] = useState([])
+  const [myUsers, setMyUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredUsers, setFilteredUsers] = useState([]);
+
   const navigate = useNavigate();
-  // console.log(user);
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
@@ -20,7 +22,7 @@ function AdminPage() {
     fetchUsers();
   }, []);
 
-   const fetchUsers = async () => {
+  const fetchUsers = async () => {
     try {
       const res = await api.get("/api/users");
       setMyUsers(res.data);
@@ -29,112 +31,116 @@ function AdminPage() {
     }
   };
 
+  //  Filtrage automatique à chaque saisie
+  useEffect(() => {
+    const term = searchTerm.toLowerCase();
+    const filtered = myUsers.filter(
+      (user) =>
+        user.nom?.toLowerCase().includes(term) ||
+        user.email?.toLowerCase().includes(term) ||
+        user.role?.toLowerCase().includes(term)
+    );
+    setFilteredUsers(filtered);
+  }, [searchTerm, myUsers]);
 
-
-// const handleDelete = async (id) => {
-
-//   if (!window.confirm("Voulez-vous vraiment supprimer cet utilisateur ?")) return;
-
-//   try {
-//     const token = localStorage.getItem("token");
-//     await api.delete(`/api/users/${id}`, {
-//       headers: { Authorization: `Bearer ${token}` },
-//     });
-
-//     navigate("/admin");
-//     fetchUsers();
-//   } catch (err) {
-//     console.error("Erreur lors de la suppression :", err);
-//     alert("Erreur lors de la suppression, veuillez réessayer.");
-//   }
-// };
-
-
-
-  return (<>
-    <Header navigate={navigate} user={user} />
-    <div className="overflow-hidden rounded-lg border border-gray-200 shadow-sm">
-      <div className="overflow-x-auto">
-        <table className="min-w-full divide-y divide-gray-200">
-     <thead className="bg-blue-600">
-  <tr>
-    <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-      N°
-    </th>
-    <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider ">
-      Nom
-    </th>
-    <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider ">
-      Email
-    </th>
-    <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
-      Rôle
-    </th>
-    <th className="px-4 py-3 text-right text-xs font-medium text-white uppercase tracking-wider">
-      Date d'inscription
-    </th>
-    <th className="px-4 py-3 text-right text-xs font-medium text-white uppercase tracking-wider">
-      Actions
-    </th>
-  </tr>
-</thead>
-<tbody className="bg-white divide-y divide-gray-200">
-  {myUsers.length > 0 ? (
-    myUsers.map((u, index) => (
-      <tr key={u._id} className="hover:bg-gray-50 cursor-pointer">
-        <td className="px-4 py-4 whitespace-nowrap">
-          <div className="flex items-center">
-            <div className="ml-4">
-              <div className="text-sm font-medium text-gray-900">
-                {index + 1}
-              </div>
-            </div>
-          </div>
-        </td>
-        <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 ">
-          <div>{u.nom}</div>
-        </td>
-        <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 ">
-          <div>{u.email}</div>
-        </td>
-        <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
-          <div>{u.role}</div>
-        </td>
-        <td className="px-4 py-4 text-right whitespace-nowrap text-sm text-gray-500">
-          <div>{formatDate(u.date_inscription)}</div>
-        </td>
-        <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
-          <div className="flex justify-end space-x-2">
-                  <Link
-                      to={`/editUser/${u._id}`}
-                      className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg transition"
-                    >
-                      modifier
-                    </Link>            <button className="text-red-600 bg-red-600 text-white rounded-lg hover:text-red-900 p-1"
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                handleDeleteUser(u._id,navigate,fetchUsers);
-              }}
-            >Supprimer</button>
-          </div>
-        </td>
-      </tr>
-    ))
-  ) : (
-    <tr>
-      <td colSpan="6" className="px-4 py-6 text-center text-gray-500">
-        Aucun invité trouvé
-      </td>
-    </tr>
-  )}
-</tbody>
-
-
-        </table>
+  return (
+    <>
+      <Header navigate={navigate} user={user} />
+      <div className="overflow-hidden rounded-lg border border-gray-200 shadow-sm ">
+        <input
+          type="text"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Rechercher un utilisateur ..."
+          className="my-10 ml-5 mr-15 w-64 sm:w-64 h-10 border border-gray-400 rounded-lg px-3 py-2 text-base text-gray-800 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-blue-400"
+        />
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y divide-gray-200">
+            <thead className="bg-blue-600">
+              <tr>
+                <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  N°
+                </th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider ">
+                  Nom
+                </th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider ">
+                  Email
+                </th>
+                <th className="px-4 py-3 text-left text-xs font-medium text-white uppercase tracking-wider">
+                  Rôle
+                </th>
+                <th className="px-4 py-3 text-right text-xs font-medium text-white uppercase tracking-wider">
+                  Date d'inscription
+                </th>
+                <th className="px-4 py-3 text-right text-xs font-medium text-white uppercase tracking-wider">
+                  Actions
+                </th>
+              </tr>
+            </thead>
+            <tbody className="bg-white divide-y divide-gray-200">
+              {filteredUsers.length > 0 ? (
+                filteredUsers.map((u, index) => (
+                  <tr key={u._id} className="hover:bg-gray-50 cursor-pointer">
+                    <td className="px-4 py-4 whitespace-nowrap">
+                      <div className="flex items-center">
+                        <div className="ml-4">
+                          <div className="text-sm font-medium text-gray-900">
+                            {index + 1}
+                          </div>
+                        </div>
+                      </div>
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 ">
+                      <div>{u.nom}</div>
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500 ">
+                      <div>{u.email}</div>
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-sm text-gray-500">
+                      <div>{u.role}</div>
+                    </td>
+                    <td className="px-4 py-4 text-right whitespace-nowrap text-sm text-gray-500">
+                      <div>{formatDate(u.date_inscription)}</div>
+                    </td>
+                    <td className="px-4 py-4 whitespace-nowrap text-right text-sm font-medium">
+                      <div className="flex justify-end space-x-2">
+                        <Link
+                          to={`/editUser/${u._id}`}
+                          className="bg-yellow-400 hover:bg-yellow-500 text-white font-semibold py-2 px-4 rounded-lg transition"
+                        >
+                          modifier
+                        </Link>{" "}
+                        <button
+                          className="text-red-600 bg-red-600 text-white rounded-lg hover:text-red-900 p-1"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            e.stopPropagation();
+                            handleDeleteUser(u._id, navigate, fetchUsers);
+                          }}
+                        >
+                          Supprimer
+                        </button>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+              ) : (
+                <tr>
+                  <td
+                    colSpan="6"
+                    className="px-4 py-6 text-center text-gray-500"
+                  >
+                    Aucun invité trouvé
+                  </td>
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </div>
-    </div></>
-  )
+    </>
+  );
 }
 
-export default AdminPage
+export default AdminPage;
